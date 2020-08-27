e358cc53355a2b31f0d392844e9a7cd14a28ab98
Connor Lysek <cjlysek@gmail.com>
2020-08-27 17:48:00 -0400

    First Version

---
+++ src/HelloWorld.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class HelloWorld{
+        public static void main(String[] args){
+        System.out.println("in main");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a name: ");
+        String name =  scan.nextLine();
+
+        System.out.printf("%s", name);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//modules
+//packages
+//class
+//methods
